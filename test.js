d5d921ea80c683eb6a4872b6bb27628b15f1de7d
b-nixon <bnixon1@seattleu.edu>
2026-06-04 14:21:13 -0700

    t

--- test.js
+++ test.js
@@ -141,8 +141,10 @@ async function testRefactor(code) {
 
 async function AIRefactor(code) {
   try {
+    const API_KEY = "AQ.Ab8RN6J6ThrmryTfKI5pySxa6TIBgsiYPT4a-lvv9FnPXBFSAw";
+
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AQ.Ab8RN6LiBt6fEKbLr3NRMtOIQJDXAf6IvbXJUHDay4eOUn5emA",
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
